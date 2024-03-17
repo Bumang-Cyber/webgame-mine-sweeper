7a2aabb00eef5c5d5ac4612c5003decf8523a05a
Bumang-Cyber <jove0729@naver.com>
2024-03-18 08:41:20 +0900

    chore: 불필요한 주석 제거

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,24 +1,11 @@
-// import { useState } from "react";
 import styled from "styled-components";
 
 import Dropdown from "./Dropdown";
 
-// import { IoVolumeMedium } from "react-icons/io5";
-// import { IoVolumeMute } from "react-icons/io5";
-import { AiOutlineColumnWidth } from "react-icons/ai";
-
 const Option = () => {
-  // const [isSoundOn, setIsSoundOn] = useState(true);
-
   return (
     <OptionContainer>
       <Dropdown />
-      <RightOption>
-        <IconButton>
-          <AiOutlineColumnWidth />
-        </IconButton>
-        {/* <IconButton>{isSoundOn ? <IoVolumeMedium /> : <IoVolumeMute />}</IconButton> */}
-      </RightOption>
     </OptionContainer>
   );
 };
@@ -32,18 +19,4 @@ export const OptionContainer = styled.section`
 
   display: flex;
   justify-content: space-between;
-`;
-
-export const RightOption = styled.div`
-  display: flex;
-  gap: 8px;
-`;
-
-export const IconButton = styled.button`
-  ${({ theme }) => theme.font.subtitle5r}
-  border-radius: 4px;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.color.lightGray300};
-  }
 `;
